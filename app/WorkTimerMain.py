3cb141513d392daaf80f3a40794f971517cc1931
snaiper04ek353 <snaiper04ek353@gmail.com>
2021-05-22 01:32:29 +0300

    complete base functional: count time for work and rest in cycle

--- app/WorkTimerMain.py
+++ app/WorkTimerMain.py
@@ -10,33 +10,44 @@ def main():
     Needs for translate seconds -> minutes, and minutes -> hours
     """
 
+    def stop_program():
+        print("Вы остановили программу.")
+        sys.exit()
+
     def start_program(input_from_kboard):
         """
-        :param input_from_kboard:
-        :return:
+        Check input from keyboard and exit program if input == q or Q, else keep executing
         """
-        if input_from_kboard == 1:
-                # "q" or "Q":
-            print("Вы остановили программу.")
-            # flag = False
-            sys.exit()
+        if input_from_kboard == "q":
+            stop_program()
+        elif input_from_kboard == "Q":
+            stop_program()
         else:
             now = datetime.datetime.now().strftime('%H:%M:%S')
             print(f"Начало выполнения программы: {now}.")
-            flag = True
-        return flag
 
     def time_counter_for_work(time_for_work, seconds=0, minutes=0, hours=0):
         """Time counter for working time wich count minutes to the end of time_for_work and on"""
-        for second in range(time_for_work):
-            time.sleep(60)
-            seconds += 60
-            if second % 500 == 0:
+        for second in range(time_for_work + 1):
+            time.sleep(1)
+            seconds += 1
+            if seconds % 300 == 0:
                 minutes += 5
+                print(f"Прошло минут: {minutes}")
+            if seconds == time_for_work:
+                print(f"Прошло минут: {time_for_work / TIME_TRANSLATE_CONST}")
 
 
-
-            print(f"Прошло {i / 60} минут ")
+    def time_counter_for_rest(time_for_rest, seconds=0, minutes=0, hours=0):
+        """Time counter for working time wich count minutes to the end of time_for_work and on"""
+        for second in range(time_for_rest + 1):
+            time.sleep(1)
+            seconds += 1
+            if seconds % 300 == 0:
+                minutes += 5
+                print(f"Прошло минут: {minutes}")
+            if seconds == time_for_work:
+                print(f"Прошло минут: {time_for_rest / TIME_TRANSLATE_CONST}")
 
 
     input_from_kboard = str(input("Введите любую клавишу для начала или введите 'Q' для остановки:"))
@@ -44,7 +55,8 @@ def main():
     flag = start_program(input_from_kboard)
     print("flag:", f'{flag}')
 
-    while flag:
+
+    while True:
 
         seconds = 0
         minutes = seconds // TIME_TRANSLATE_CONST
@@ -52,6 +64,9 @@ def main():
 
         time_for_work = 52 * TIME_TRANSLATE_CONST
         time_for_rest = 18 * TIME_TRANSLATE_CONST
+
+        time_counter_for_work(time_for_work)
+        time_counter_for_rest(time_for_rest)
 
 
 if __name__ == '__main__':
